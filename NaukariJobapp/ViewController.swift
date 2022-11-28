5e4804a665fdbf3e1879ca89a580cb1c3732e881
bhanu734 <bhanu734@gmail.com>
2022-11-27 21:43:31 -0800

    Initial Commit

---
+++ NaukariJobapp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NaukariJobapp
+//
+//  Created by Mac on 27/11/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
